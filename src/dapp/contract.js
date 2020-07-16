8416271daad49ba9d7f99c2f406d42b4dcccc3d7
btxurruka <betxurruka@gmail.com>
2020-07-16 19:22:24 +0200

    update config files to make dapp run

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -1,4 +1,5 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import Config from './config.json';
 import Web3 from 'web3';
 
@@ -8,6 +9,7 @@ export default class Contract {
         let config = Config[network];
         this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+        this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
         this.initialize(callback);
         this.owner = null;
         this.airlines = [];
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,14 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require('truffle-hdwallet-provider');
 var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
+      // provider: function() {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
+      // },
+      host: "127.0.0.1",
+      port: 8545,
       network_id: '*',
       gas: 9999999
     }
