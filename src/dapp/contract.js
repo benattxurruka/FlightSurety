3aeb8905d465fe966f48aa5dcf9e120c8afd79a1
btxurruka <betxurruka@gmail.com>
2020-08-07 09:31:16 +0200

    add style to frontpage and add more functionalities to it

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -14,12 +14,15 @@ export default class Contract {
         this.owner = null;
         this.airlines = [];
         this.passengers = [];
+        this.accounts = [];
     }
 
     initialize(callback) {
         this.web3.eth.getAccounts((error, accts) => {
            
             this.owner = accts[0];
+
+            this.accounts = accts;
 
             let counter = 1;
             
@@ -54,5 +57,81 @@ export default class Contract {
             .send({ from: self.owner}, (error, result) => {
                 callback(error, payload);
             });
+    }
+
+    registerAirline(address, name) {
+        let self = this;
+        let payload = {
+            airlineAddress: address,
+            name: name
+        }
+        self.flightSuretyData.methods
+            .registerAirline(payload.airlineAddress, payload.name)
+            .send({ from: self.owner}, (error, result) => {
+                if(error) {
+                    console.log(error);
+                  } 
+                  else {
+                    alert('Registered: ' + payload.name);
+                    console.log('Registered: ' + payload.name);
+                    console.log(payload);
+                  }
+            });
+    }
+
+    fund(funds, callback) {
+        let self = this;
+        let payload = {
+            funds: funds
+        } 
+        self.flightSuretyData.methods
+            .fund()
+            .send({ from: self.owner, value: payload.funds}, (error, result) => {
+                callback(error, payload);
+            });
+    }
+
+    registerFlight(flight, destination, callback) {
+        let self = this;
+        console.log(self.accounts);
+        let payload = {
+            flight: flight,
+            destination: destination,
+            timestamp: Math.floor(Date.now() / 1000)
+        } 
+        self.flightSuretyApp.methods
+            .registerFlight(payload.flight, payload.destination, payload.timestamp)
+            .send({ from: self.accounts[6]}, (error, result) => {
+                callback(error, payload);
+            });
+    }
+
+    buy(flight, price, callback) {
+        let self = this;
+        let payload = {
+            flight: flight,
+            price: price
+        } 
+        self.flightSuretyData.methods
+            .buy(flight)
+            .send({ from: self.owner, value: price}, (error, result) => {
+                callback(error, payload);
+            });
+    }
+
+    getCreditToPay(callback) {
+        let self = this;
+        self.flightSuretyData.methods.
+        getCreditToPay().call({ from: self.owner}, (error, result) => {
+            callback(error, result);
+        });
+    }
+
+    pay(callback) {
+        let self = this;
+        self.flightSuretyData.methods.
+        pay().call({ from: self.owner}, (error, result) => {
+            callback(error, result);
+        });
     }
 }
--- src/dapp/index.js
+++ src/dapp/index.js
@@ -25,7 +25,79 @@ import './flightsurety.css';
                 display('Oracles', 'Trigger oracles', [ { label: 'Fetch Flight Status', error: error, value: result.flight + ' ' + result.timestamp} ]);
             });
         })
-    
+
+        // User-submitted transaction
+        DOM.elid('register-airline').addEventListener('click', (e) => {
+            e.preventDefault();
+            let address = DOM.elid('airline-address').value;
+            let name = DOM.elid('airline-name').value;
+            console.log('address: '+address+'. Name: '+name);
+            // Write transaction
+            contract.registerAirline(address, name, (error, result) => {
+                display('Airlines', 'Register a new airline', [ { label: 'Register Airline', error: error, value: result.address + ' ' + result.name} ]);
+            });
+        })
+        
+        // User-submitted transaction
+        DOM.elid('fund').addEventListener('click', () => {
+            // e.preventDefault();
+            let funds = DOM.elid('funds').value;
+            // Write transaction
+            contract.fund(funds, (error, result) => {
+                display('Funds', 'Fund yourself', [ { label: 'Fund added to airline', error: error, value: result.funds} ]);
+            });
+        })
+
+        // User-submitted transaction
+        DOM.elid('register-flight').addEventListener('click', () => {
+            let flight = DOM.elid('new-flight-number').value;
+            let destination = DOM.elid('new-flight-destination').value;
+            // Write transaction
+            contract.registerFlight(flight, destination, (error, result) => {
+                display('New Flights', 'Register a new flight', [ { label: 'New Flight info', error: error, value: result.flight + ' ' + result.destination} ]);
+            });
+        })
+
+        // User-submitted transaction
+        DOM.elid('buy-insurance').addEventListener('click', () => {
+            let flight = DOM.elid('insurance-flight').value;
+            let price = DOM.elid('insurance-price').value;
+            // Write transaction
+            contract.buy(flight, price, (error, result) => {
+                display('Buy insurance', 'Buy a new flight insurance', [ { label: 'Bought insurance info', error: error, value: result.flight} ]);
+            });
+        })
+
+        // User-submitted transaction
+        DOM.elid('check-credit').addEventListener('click', () => {
+            // Write transaction
+            contract.getCreditToPay((error, result) => {
+                if(error){
+                    console.log(error);
+                    let creditDisplay = DOM.elid("credit-ammount");
+                    creditDisplay.value = "Error happened while getting your credit";
+                } else {
+                    let creditDisplay = DOM.elid("credit-ammount");
+                    creditDisplay.value = result+" ethers";
+                }
+            });
+        })
+
+        // User-submitted transaction
+        DOM.elid('claim-credit').addEventListener('click', () => {
+            // Write transaction
+            contract.pay((error, result) => {
+                if(error){
+                    console.log(error);
+                    let creditDisplay = DOM.elid("credit-ammount");
+                } else {
+                    let creditDisplay = DOM.elid("credit-ammount");
+                    creditDisplay.value = "0 ethers";
+                    alert("Successfully withdrawed!");
+                }
+            });
+        })
+        
     });
     
 
